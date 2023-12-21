2d5418f1b30ea5e480d4f8e4fbab2e4611769fe3
MUTHUAKALYA <muthuakalyaa@gmail.com>
2023-12-21 14:47:01 +0530

    new feature added

--- javascript - DOM/01)Form/assets/js/main.js
+++ javascript - DOM/01)Form/assets/js/main.js
@@ -1,25 +1,20 @@
-// const formContainerEl=document.getElementById("form-container");
-// const studentNameEl=document.getElementById("studentname");
-// const registerNumEl=document.getElementById("register-no");
-// const dobEl=document.getElementById("dob");
-// const mobileNumberEl=document.getElementById("mobile-no");
-// const gmailEl=document.getElementById("gmail");
-// const addressEl=document.getElementById("Address");
-// const districtEl=document.getElementById("District");
-// const submitButtonEl=document.getElementById("submit-button");
-
-// const employeeIdEls = formEl.elements.employeeId.value
 
 const formContainerEl = document.forms["Souvenir-details"]
 const submitButtonEl=document.getElementById("submit-button");
 const outputContentEl=document.getElementById("outputContent")
 const outputEl = document.getElementById("output");
-const studentElerror=document.getElementById("studentElerror")
+const thankyouEl= document.getElementById("thankyou")
+const secondSubmitButtonEl=document.getElementById("secondSubmit")
 
+console.log(secondSubmitButtonEl);
 
 document.addEventListener("DOMContentLoaded",()=>{
     outputEl.style.display="none";
+    thankyouEl.style.display="none";
 })
+
+
+
 
 submitButtonEl.addEventListener("click",(e)=>{
     e.preventDefault();
@@ -51,81 +46,16 @@ submitButtonEl.addEventListener("click",(e)=>{
     
 })
 
+secondSubmitButtonEl.addEventListener("click",()=>{
+    outputEl.style.display="none";
+    thankyouEl.style.display="block";
+})
 
 
 
 
 
 
-// const firstErrorMsgEl=document.getElementById("first-error");
-// const secondErrorMsgEl=document.getElementById("second-error");
-// const thirdErrorMsgEl=document.getElementById("third-error");
-// const fourthErrorMsgEl=document.getElementById("fourth-error");
-// const fifthErrorMsgEl=document.getElementById("fifth-error");
-// const sixthErrorMsgEl=document.getElementById("sixth-error")
-// const seventhErrorMsgEl=document.getElementById("seventh-error")
-// document.addEventListener("DOMContentLoaded",()=>{
-//     firstErrorMsgEl.style.display="none";
-//     secondErrorMsgEl.style.display="none";
-//     thirdErrorMsgEl.style.display="none";
-//     fourthErrorMsgEl.style.display="none";
-//     fifthErrorMsgEl.style.display="none";
-//     sixthErrorMsgEl.style.display="none";
-//     seventhErrorMsgEl.style.display="none";
-    
-// })
-
-// submitButtonEl.addEventListener("click",(e)=>{
-//      e.preventDefault();
-//     if(studentNameEl === null || studentNameEl === ""){
-//         firstErrorMsgEl.style.display="block"
-//     }
-//     else{
-//         firstErrorMsgEl.style.display="none"
-//     }
-//     if(registerNumEl === null || registerNumEl === ""){
-//         secondErrorMsgEl.style.display="block"
-//     }
-//     else{
-//         secondErrorMsgEl.style.display="none"
-//     }
-//     if(dobEl === null ||dobEl === ""){
-//         thirdErrorMsgEl.style.display="block"
-//     }else{
-//         thirdErrorMsgEl.style.display="none"
-//     }
-//     if(mobileNumberEl === null || mobileNumberEl === ""){
-//         fourthErrorMsgEl.style.display="block"
-//     }else{
-//         fourthErrorMsgEl.style.display="none"
-//     }
-//     if(gmailEl.value === null || gmailEl.value === ""){
-//         fifthErrorMsgEl.style.display="block"
-//     }else{
-//         fifthErrorMsgEl.style.display="none"
-//     }
-//     if(addressEl === null || addressEl === ""){
-//         sixthErrorMsgEl.style.display="block"
-//     }else{
-//         sixthErrorMsgEl.style.display="none"
-//     }
-//     if(studentNameEl === null || studentNameEl === ""){
-//         seventhErrorMsgEl.style.display="block"
-//     }else{
-//         seventhErrorMsgEl.style.display="none"
-//     }
-   
-// })
 
 
 
-// submitButtonEl.addEventListener("click",(e)=>{
-//     console.log(studentNameEl.value)
-//     console.log(registerNumEl.value)
-//     console.log(dobEl.value);
-//     console.log(mobileNumberEl.value)
-//     console.log(gmailEl.value)
-//     console.log(addressEl.value)
-//     console.log(districtEl.value)
-//     e.preventDefault()
-// })
--- javascript - DOM/01)Form/dist/assets/index-OSnzgfGF.js
+++
@@ -1,9 +0,0 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&r(o)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const n=document.forms["Souvenir-details"],m=document.getElementById("submit-button"),a=document.getElementById("outputContent"),c=document.getElementById("output");document.getElementById("studentElerror");document.addEventListener("DOMContentLoaded",()=>{c.style.display="none"});m.addEventListener("click",i=>{i.preventDefault();const s=n.elements.studentname.value,l=n.elements.register.value,r=n.elements.dob.value,e=n.elements.mobile.value,t=n.elements.gmail.value,o=n.elements.Address.value,u=n.elements.District.value;if(s.length>0&&l.length>0&&r.length>0&&e.length>0&&t.length>0&&o.length>0&&u.length>0){const d=`
-            <p><strong>Full Name</strong> : ${s}</p>
-            <p><strong>Register Number</strong> : ${l}</p>
-            <p><strong>DOB</strong> : ${r}</p>
-            <p><strong>Mobile Number</strong> : ${e}</p>
-            <p><strong>Gmail</strong> : ${t}</p>
-            <p><strong>Address</strong> : ${o}</p>
-            <p><strong>District</strong> : ${u}</p>
-        `;a.innerHTML=d,n.style.display="none",c.style.display="block"}});
