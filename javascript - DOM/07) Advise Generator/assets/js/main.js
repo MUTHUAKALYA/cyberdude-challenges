fc1c6a42eae3ad37ef4082e44adcaa7f8e9d40bb
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-01-13 14:56:54 +0530

    desing completed

---
+++ javascript - DOM/07) Advise Generator/assets/js/main.js
@@ -0,0 +1,27 @@
+const quoteEl = document.querySelector("#quote");
+const apiUrl = "https://api.adviceslip.com/advice";
+
+const buttonEl = document.querySelector("#button")
+buttonEl.style.display = "none";
+
+// reload btn
+buttonEl.addEventListener("click", () => {
+    window.location.href = "./";
+});
+
+// async function  
+async function getAdvicesData() {
+    try {
+        const response = await fetch(apiUrl);
+        const jsonData = await response.json();
+        return jsonData;
+    } catch (error) {
+console.error("Network error: ", error);
+    }
+}
+
+const adviceData = await getAdvicesData();
+quoteEl.textContent = adviceData.slip.advice;
+
+// reload btn view
+buttonEl.style.display = "block";
--- javascript - DOM/07) Advise Generator/tailwind.config.js
+++ javascript - DOM/07) Advise Generator/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        custom: ['Concert One', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
