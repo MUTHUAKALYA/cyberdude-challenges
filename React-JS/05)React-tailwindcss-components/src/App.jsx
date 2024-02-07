68686b0e0242b4c152727e4303ede7bdfb8c9e8a
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-07 07:23:50 +0530

    importing ButtonsPage

--- React-JS/05)React-tailwindcss-components/src/App.jsx
+++ React-JS/05)React-tailwindcss-components/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import AlertPage from './Pages/AlertPage'
+import ButtonsPage from './Pages/ButtonsPage'
 
 const App = () => {
   return (
     <div>
         <Navbar/>
         <AlertPage/>
+        <ButtonsPage/>
     </div>
   )
 }
