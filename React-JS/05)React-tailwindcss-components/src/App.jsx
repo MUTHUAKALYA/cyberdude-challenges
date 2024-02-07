e0fb500231febdc6bbd73f38266d8cceaf25db2b
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-07 06:46:53 +0530

    importing navbar and alertpage

---
+++ React-JS/05)React-tailwindcss-components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import AlertPage from './Pages/AlertPage'
+
+const App = () => {
+  return (
+    <div>
+        <Navbar/>
+        <AlertPage/>
+    </div>
+  )
+}
+
+export default App
