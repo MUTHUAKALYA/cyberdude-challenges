d78c2bed2dee60bb3711437b4f107f2d0045b00d
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-07 05:36:27 +0530

    feat:adding custom colors

--- React-JS/05)React-tailwindcss-components/tailwind.config.js
+++ React-JS/05)React-tailwindcss-components/tailwind.config.js
@@ -9,6 +9,12 @@ export default {
       fontFamily:{
         oswald : ['Oswald', 'sans-serif']
         
+      },
+      colors:{
+        lavender:"#3B3C4F",
+        pistachio:"#DBE4C9",
+        coastline:"#DDEDEA",
+        flour:"#F7F4EB"
       }
     },
   },
