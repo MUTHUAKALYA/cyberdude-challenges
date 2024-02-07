164a5b667445a76d13d0363391f5df1069cc49b0
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-07 06:50:01 +0530

    feat:Creating AlertPage in Pages folder

---
+++ React-JS/05)React-tailwindcss-components/src/Pages/AlertPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Alert from '../Components/Alert'
+
+const AlertPage = () => {
+  return (
+    <div className='max-w-2xl p-10'>
+        <h2 className='text-center font-semibold text-xl'>Buttons</h2>
+       <Alert color="bg-blue-200" border="border-blue-400">
+       <p>A simple Primary Alert </p>
+       </Alert>
+       <Alert color="bg-gray-300" border="border-gray-500">
+       <p>A simple Secondary Alert </p>
+       </Alert>
+       <Alert color="bg-green-300" border="border-green-500">
+       <p>A simple Sucess Alert </p>
+       </Alert>
+       <Alert color="bg-red-300" border="border-red-500">
+       <p>A simple danger Alert </p>
+       </Alert>
+       <Alert color="bg-yellow-200" border="border-yellow-400">
+       <p>A simple Warning Alert </p>
+       </Alert>
+       <Alert color="bg-sky-200" border="border-sky-400">
+       <p>A simple Info Alert </p>
+       </Alert>
+       <Alert color="bg-gray-100" border="border-gray-300">
+       <p>A simple light Alert </p>
+       </Alert>
+       <Alert color="bg-gray-400" border="border-gray-600">
+       <p>A simple dark Alert </p>
+       </Alert>
+    </div>
+  )
+}
+
+export default AlertPage
