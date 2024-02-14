959144f2ab4d239a065aaa0b09ada9f049eef7e4
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-14 17:33:21 +0530

    adding dist folder

--- React-JS/06)ReactForm/src/App.jsx
+++ React-JS/06)ReactForm/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ContactPage from "./Pages/ContactPage";
 import Header from "./Components/header";
+import ContactPageCopy from "./Pages/ContactPageCopy";
 
 const App = () => {
   return (
     <>
       <Header />
-      <ContactPage />
+      {/* <ContactPage /> */}
+      <ContactPageCopy/>
     </>
   );
 };
