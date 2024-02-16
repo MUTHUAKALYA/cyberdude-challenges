e3a47fb37f6315996698f312d442b8de033cf266
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-16 23:10:26 +0530

    feat: adding deleting feature

--- React-JS/08) Add-input-in-ui/src/App.jsx
+++ React-JS/08) Add-input-in-ui/src/App.jsx
@@ -6,12 +6,19 @@ const App = () => {
   const handleSubmit = (e) => {
     // preventing page refresh
     e.preventDefault();
-    
+
     console.log(value);
-    setTask( task.concat([value]))
-   
+    setTask(task.concat([value]));
+
     //resetting the input field after submitting the form
-    setValue('')
+    setValue("");
+  };
+
+  const removeTask = (idToRemove) => {
+    const updateArray = task.filter((task, index) => {
+      return index !== idToRemove;
+    });
+    setTask(updateArray);
   };
   return (
     <section className="h-screen bg-teal-800 p-10 ">
@@ -36,21 +43,32 @@ const App = () => {
         </form>
 
         <ul className="">
-          {task.map((task,index)=>{
-            return (<><li className="bg-white px-5 py-3 w-[full]" key={index}>{`${index+1}) ${task}`}</li>
-           {/* <button className="" ><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275z"></path></svg></button> */}
-            </>
-            )
+          {task.map((task, index) => {
+            return (
+              <div className="bg-white px-5 py-3 w-[full] flex justify-between">
+                <span className=" " key={index}>{`${
+                  index + 1
+                }) ${task}`}</span>
+                <button className="" onClick={() => removeTask(index)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275z"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            );
           })}
         </ul>
       </div>
-
-
-      
     </section>
-  
   );
- 
 };
 
 export default App;
