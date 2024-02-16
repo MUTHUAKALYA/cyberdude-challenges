966e7d8eee6206cf7f4f8280e8bb53c69f38b704
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-16 21:31:34 +0530

    feat: updating values in ui

--- React-JS/08) Add-input-in-ui/src/App.jsx
+++ React-JS/08) Add-input-in-ui/src/App.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [value, setValue] = useState("");
+  const [task, setTask] = useState([]);
+  const handleSubmit = (e) => {
+    // preventing page refresh
+    e.preventDefault();
+    
+    console.log(value);
+    setTask( task.concat([value]))
+   
+    //resetting the input field after submitting the form
+    setValue('')
+  };
   return (
-    <div>App</div>
-  )
-}
+    <section className="h-screen bg-teal-800 p-10 ">
+      <div className="bg-white  p-10 max-w-3xl mx-auto rounded-lg bg-opacity-70 backdrop-blur-0 shadow-2xl">
+        <h1 className="font-black text-xl text-center">To - Do List</h1>
 
-export default App
+        <form className="py-3" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add task"
+            name="addTask"
+            className=" py-2 px-5 w-[85%] outline-none text-gray-400"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button
+            className="bg-fuchsia-950  py-2 w-[15%] text-white hover:bg-fuchsia-800"
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
+
+        <ul className="">
+          {task.map((task,index)=>{
+            return (<><li className="bg-white px-5 py-3 w-[full]" key={index}>{`${index+1}) ${task}`}</li>
+           {/* <button className="" ><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275z"></path></svg></button> */}
+            </>
+            )
+          })}
+        </ul>
+      </div>
+
+
+      
+    </section>
+  
+  );
+ 
+};
+
+export default App;
