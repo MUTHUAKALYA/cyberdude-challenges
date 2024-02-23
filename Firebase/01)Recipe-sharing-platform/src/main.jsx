6b95b7312d559c1b62adb3f5efd214500cf5b533
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-24 05:27:10 +0530

    feat:creating router

--- Firebase/01)Recipe-sharing-platform/src/main.jsx
+++ Firebase/01)Recipe-sharing-platform/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import General from './Layout/General.jsx'
+import Homepage from './Pages/Homepage.jsx'
+import Veg from './Pages/Veg.jsx'
+import NonVeg from './Pages/NonVeg.jsx'
+import Vegan from './Pages/Vegan.jsx'
+
+const router = createBrowserRouter([{
+  path:"/",
+  element:<General/>,
+  children:[{
+    path:"/",
+    element:<Homepage/>
+  },
+{
+  path:"/veg",
+  element:<Veg/>
+},
+{
+  path:"/nonveg",
+  element:<NonVeg/>
+},
+{
+  path:"/vegan",
+  element:<Vegan/>
+}]
+}])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}></RouterProvider>
 )
