0c4ed6f4b666c783d1ec9ade76b3efde43516bc4
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-24 05:34:30 +0530

    feat: creating homepage

--- Firebase/01)Recipe-sharing-platform/src/Pages/Homepage.jsx
+++ Firebase/01)Recipe-sharing-platform/src/Pages/Homepage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import HeroSection from '../Components/HeroSection'
+import Form from '../Components/Form'
+import Newsletter from '../Components/Newsletter'
+import Cards from '../Components/Cards'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+    <HeroSection/>
+    <Cards/>
+    <Form/>
+    <Newsletter/>
+    </>
   )
 }
 
