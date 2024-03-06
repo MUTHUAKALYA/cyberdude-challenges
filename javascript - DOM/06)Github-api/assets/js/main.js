07e355399e58ec9b92103b176fd75cbcc78172f9
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-03-06 12:28:58 +0530

    BREAKING CHANGE:using fetch method instead of xmlhttpsrequest

--- javascript - DOM/06)Github-api/assets/js/main.js
+++ javascript - DOM/06)Github-api/assets/js/main.js
@@ -1,18 +1,16 @@
 // creating new object
 
 const internsUserName = [
-"muthukumarimoorthi",
-"mshajid",
-"swethadsalvatore",
-"mushkir",
-"gayathrihg",
-"yrd369",
-"esakki2104prsnl",
-  "Vk2401",
-  "suriyamassmsd",
+  "mushkir",
+  "mshajid",
+  "gayathrihg",
+  "yrd369",
+  "swethadsalvatore",
+  "muthukumarimoorthi",
   "riyaz1000",
   "hema-venkat3",
   "bearcin46",
+  "suriyamassmsd",
   "dineshdevelope",
   "jeya-rosini",
   "muthuakalya",
@@ -22,31 +20,81 @@ const internsUserName = [
   "kishorekumar-kp",
 ];
 
-// Variable cannot be accessed, XHR (based on callbacks not promises)
-
 function fetchIntens(user) {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      const data = JSON.parse(xhr.responseText);
-      console.log(data.avatar_url);
-
+  const api = `https://api.github.com/users/${user}`;
+  fetch(api)
+    .then((response) => response.json())
+    .then((data) => {
       const cardContainerEl = document.getElementById("card-container");
       const cardEl = document.createElement("div");
       cardEl.className = "cardel";
       cardEl.innerHTML = `
-    <img src="${data.avatar_url}"  alt="${data.login}-image" class="imgel">
-      <h3 class="username">${data.name} </h3>
-    <span class="role">Full Stack Engineer</span>
-       <span class="location">${data.location}</span>
-       <button class="button"><a href="${data.html_url}" target="_blank">View - Github</a></button>
-        `;
+        <img src="${data.avatar_url}"  alt="${data.login}-image" class="imgel">
+          <h3 class="username">${data.name} </h3>
+           <span class="role">Full Stack Engineer</span> 
+           <span class="location">${
+             data.location ? data.location : "Tamilnadu , India"
+           }</span>
+           <div class="details">
+           <div class="followersel">
+           <span>${data.followers}</span>
+           <span>Followers</span>
+           </div>
+           <div class="followersel">
+           <span>${data.public_repos}</span>
+           <span>Public Repositories</span>
+           </div>
+           </div>
+          <div class="buttonContainer">
+          <button class="button"><a href="${
+            data.html_url
+          }" target="_blank">View Github</a></button>
+          <button class="button"><a href="https://github.com/${
+            data.login
+          }?tab=repositories" target="_blank">View All Repositories</a></button>
+          </div>
+            `;
       cardContainerEl.append(cardEl);
-    }
-  };
-  xhr.open("GET", `https://api.github.com/users/${user}`);
-  xhr.send();
+    });
 }
+
+// The following code uses XMLHttpRequest (yes,its going to be deprecated . Working on this method to get familiar with what is been used before the introduction of fetch)
+
+// Variable cannot be accessed, XHR (based on callbacks not promises)
+
+// function fetchIntens(user) {
+//   const xhr = new XMLHttpRequest();
+//   xhr.onreadystatechange = function () {
+//     if (this.readyState === 4 && this.status === 200) {
+//       const data = JSON.parse(xhr.responseText);
+//       console.log(data.avatar_url);
+
+//       const cardContainerEl = document.getElementById("card-container");
+//       const cardEl = document.createElement("div");
+//       cardEl.className = "cardel";
+//       cardEl.innerHTML = `
+//     <img src="${data.avatar_url}"  alt="${data.login}-image" class="imgel">
+//       <h3 class="username">${data.name} </h3>
+//     <span class="role">Full Stack Engineer</span>
+//        <span class="location">${data.location}</span>
+//        <div class="details">
+//        <div class="followersel">
+//        <span>${data.followers}</span>
+//        <span>Followers</span>
+//        </div>
+//        <div class="followersel">
+//        <span>${data.public_repos}</span>
+//        <span>Public Repositories</span>
+//        </div>
+//        </div>
+//        <button class="button"><a href="${data.html_url}" target="_blank">View - Github</a></button>
+//         `;
+//       cardContainerEl.append(cardEl);
+//     }
+//   };
+//   xhr.open("GET", `https://api.github.com/users/${user}`);
+//   xhr.send();
+// }
 
 internsUserName.forEach((intern) => fetchIntens(intern));
 console.log(internsUserName);
