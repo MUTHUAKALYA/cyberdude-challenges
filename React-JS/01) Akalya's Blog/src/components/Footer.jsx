68c9ee1aaa0e2bca7e90efa481cfa3b31c77edfd
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-03-06 16:21:59 +0530

    feat:responsive footer

--- React-JS/01) Akalya's Blog/src/components/Footer.jsx
+++ React-JS/01) Akalya's Blog/src/components/Footer.jsx
@@ -3,10 +3,10 @@ const Footer = ()=>{
     return(
         <>
         <footer className="bg-lightSecondary">
-            <section className="flex justify-between py-5 px-10 bg-darkPrimary text-white">
-                <h3 className="font-bold text-xl">Akalya Blogs</h3>
+            <section className="flex flex-col justify-center md:flex-row md:justify-between py-5 px-10 bg-darkPrimary text-white gap-y-4 md:gap-y-0">
+                <h3 className="font-bold text-xl text-center">Akalya Blogs</h3>
                 <div>
-                    <ul className="flex gap-x-8 font-semibold">
+                    <ul className="flex items-center justify-center gap-x-8 font-semibold">
                         <li><Link to="/" className="hover:text-lightSecondary">Home</Link></li>
                         <li><Link to="/blogs" className="hover:text-lightSecondary">Blogs</Link></li>
                         {/* <li><Link to="/about" className="hover:text-lightSecondary">About</Link></li> */}
